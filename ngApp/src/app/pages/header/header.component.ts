e6356f96dd77643e82ad4539ca34fd80fddf9092
Lucas Pitol <lucascpitol0@gmil.com>
2019-08-15 19:28:38 -0300

    Header (Progress)

---
+++ ngApp/src/app/pages/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: 'header-component',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css'],
+	// providers: [DashboardService]
+})
+
+export class HeaderComponent {
+	
+}
