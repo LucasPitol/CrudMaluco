9e6afa61065537973ae32db0b2621f8e947a10a7
Pitol <lucascpitol0@gmail.com>
2019-08-21 23:41:48 -0300

    Home

--- ngApp/src/app/pages/header/header.component.ts
+++ ngApp/src/app/pages/header/header.component.ts
@@ -1,4 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+export enum TabsEnum {
+	HOME = 1,
+	BEST_SALLERS = 2,
+	PROFIT = 3,
+}
+
 
 @Component({
 	selector: 'header-component',
@@ -7,6 +14,30 @@ import { Component } from "@angular/core";
 	// providers: [DashboardService]
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+	tytle = 'Crud Maluco'
+
+	currentTab: number
+
+	HOME_TAB = TabsEnum.HOME
+ 	BEST_SALLERS_TAB = TabsEnum.BEST_SALLERS
+ 	PROFIT_TAB = TabsEnum.PROFIT
+
+	tabs = [
+        { label: 'Home', id: this.HOME_TAB },
+        { label: 'bestSallers', id: this.BEST_SALLERS_TAB },
+        { label: 'profit', id: this.PROFIT_TAB },
+    ];
+	
+	ngOnInit(): void {
+		this.currentTab = this.HOME_TAB;
+
+		console.log(this.HOME_TAB)
+		console.log(this.currentTab)
+	}
+
+	constructor() {
+		
+	}
 	
 }
