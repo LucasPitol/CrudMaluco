b9d031f7d10917c5019c76ffa209bb38bad1581a
Lucas Pitol <lucascpitol0@gmil.com>
2019-09-10 17:30:52 -0300

    Delete

--- goService/main.go
+++ goService/main.go
@@ -64,7 +64,17 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
+	params := mux.Vars(r)
+
+	for index, item := range books {
+		if item.Id == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func main() {
