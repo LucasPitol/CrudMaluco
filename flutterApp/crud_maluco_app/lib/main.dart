fa11911889b8d8852fdd034a404aff37a9e679fd
Pitol <lucascpitol0@gmail.com>
2020-02-23 19:06:05 -0300

    CD-DEV-5 tarefa 3 Progress

--- flutterApp/crud_maluco_app/lib/main.dart
+++ flutterApp/crud_maluco_app/lib/main.dart
@@ -21,6 +21,27 @@ class _CDState extends State<MyApp> {
             'Crud Maluco',
             style: TextStyle(color: Colors.black),
           ),
+          leading: GestureDetector(
+            onTap: () {},
+            child: Icon(
+              Icons.filter_list,
+              color: Colors.black,
+            ),
+          ),
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                right: 20,
+              ),
+              child: GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Icons.info,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ],
         ),
         body: Column(
           children: [
