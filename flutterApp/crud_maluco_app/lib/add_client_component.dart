439c06a8ba4b6ff40a463a2c91581accb1f821dc
Pitol <lucascpitol0@gmail.com>
2020-02-26 12:16:36 -0300

    CD-DEV-7 tarefa 2 refactor

--- flutterApp/crud_maluco_app/lib/add_client_component.dart
+++ flutterApp/crud_maluco_app/lib/add_client_component.dart
@@ -1,9 +1,26 @@
+import 'package:crud_maluco_app/main.dart';
 import 'package:flutter/material.dart';
 
 class AddClientComponent extends MaterialPageRoute<String> {
   AddClientComponent()
       : super(builder: (BuildContext context) {
           return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              title: Text(
+                'Crud Maluco',
+                style: TextStyle(color: Colors.black),
+              ),
+              leading: GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.black,
+                ),
+              ),
+            ),
             body: Column(
               children: <Widget>[
                 Container(
