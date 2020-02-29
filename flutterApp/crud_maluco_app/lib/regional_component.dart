96cf760aa05137b4106540e487ade8670cf485e5
Pitol <lucascpitol0@gmail.com>
2020-02-28 22:48:26 -0300

    CD-DEV-10 tarefa 3 progress

--- flutterApp/crud_maluco_app/lib/regional_component.dart
+++ flutterApp/crud_maluco_app/lib/regional_component.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class _RegionalData {
+  final String city;
+  final int totalClients;
+
+  _RegionalData(this.city, this.totalClients);
+}
 
 class RegionalComponent extends StatelessWidget {
+  List<_RegionalData> _pieData = [
+    _RegionalData('Rio de Janeiro', 20),
+    _RegionalData('São Paulo', 50),
+    _RegionalData('Minas Gerais', 10),
+    _RegionalData('Acre', 2),
+  ];
+
+  final _colorPalettes = charts.MaterialPalette.getOrderedPalettes(4);
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Regional'),
+      child: charts.PieChart(
+        [
+          charts.Series<_RegionalData, String>(
+            id: 'id do bagulho',
+            colorFn: (_, idx) => _colorPalettes[idx].shadeDefault,
+            domainFn: (_RegionalData regional, _) => regional.city,
+            measureFn: (_RegionalData regional, _) => regional.totalClients,
+            data: this._pieData,
+            labelAccessorFn: (_RegionalData row, _) =>
+                '${row.city}: ${row.totalClients}',
+          ),
+        ],
+        animate: true,
+      ),
     );
   }
 }
